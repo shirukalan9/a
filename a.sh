02866787ea49c980bcf7d222daf3acafb3658db0
newestzdn <127198882+newestzdn@users.noreply.github.com>
2026-04-13 15:07:10 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,8 +7,18 @@ source build/envsetup.sh
 # Device 
 
 git clone -b axion https://github.com/zaidanprjkt/device_infinix_X6882 device/infinix/X6882
+git clone https://github.com/zaidanprjkt/vendor_infinix_X6882-2 vendor/infinix/X6882
 
+----------------------------------------------------
+# Mediatek Specific
+git clone -b sixteen-qpr2 https://github.com/MillenniumOSS/android_vendor_mediatek_ims vendor/mediatek/ims
+git clone https://github.com/MillenniumOSS/android_hardware_mediatek hardware/mediatek
 
+-----------------------------------------------------
+# Build
+gk -s
+axion X6882 user va
+ax -br -j$(nproc)
 
 
 
