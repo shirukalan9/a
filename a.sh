f1039443da5be7e43b96626dc1236a29dcba34de
newestzdn <127198882+newestzdn@users.noreply.github.com>
2026-04-13 15:31:05 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,6 @@
+# Cleanup
+rm -rf device/infinix vendor kernel/infinix hardware
+
 # ROM
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --depth=1 --git-lfs
 /opt/crave/resync.sh
@@ -5,14 +8,13 @@ source build/envsetup.sh
 
 ---------------------------------------------------
 # Device 
-
 git clone -b axion https://github.com/zaidanprjkt/device_infinix_X6882 device/infinix/X6882
 git clone https://github.com/zaidanprjkt/vendor_infinix_X6882-2 vendor/infinix/X6882
 
 ----------------------------------------------------
 # Mediatek Specific
 git clone -b sixteen-qpr2 https://github.com/MillenniumOSS/android_vendor_mediatek_ims vendor/mediatek/ims
-git clone https://github.com/MillenniumOSS/android_hardware_mediatek hardware/mediatek
+git clone https://github.com/mt6789-transsion/hardware_transsion hardware/mediatek
 
 -----------------------------------------------------
 # Build
