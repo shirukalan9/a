5d5bf65991d2cc357670990dad3d0966dda23e5b
newestzdn <127198882+newestzdn@users.noreply.github.com>
2026-04-13 14:34:20 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,19 @@
+# ROM
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --depth=1 --git-lfs
+/opt/crave/resync.sh
+source build/envsetup.sh
+
+---------------------------------------------------
+# Device 
+
+git clone -b axion https://github.com/zaidanprjkt/device_infinix_X6882 device/infinix/X6882
+
+
+
+
+
+
+
+
+
+
