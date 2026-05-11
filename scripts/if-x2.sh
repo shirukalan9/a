f4c288d10c0851bfc553ce7ef97fe64201f73e44
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-05-12 05:55:09 +0700

    hh

--- scripts/if-x2.sh
+++ scripts/if-x2.sh
@@ -2,18 +2,18 @@
 rm -rf device frameworks external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
 
 # ROM Init
-#repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault --depth=1 --git-lfs --no-repo-verify
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault --depth=1 --git-lfs --no-repo-verify
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
 # Device 
-git clone -b stg-ax https://github.com/testingprjct/8dua device/infinix/X6882
+git clone -b staging  https://github.com/testingprjct/8dua device/infinix/X6882
 
 # Build
 source build/envsetup.sh
-axion X6882 user pico
-ax -br -j$(nproc --all)
+#axion X6882 user pico
+#ax -br -j$(nproc --all)
 
 
-#lunch infinity_X6882-user
-#m bacon -j$(nproc --all)
+lunch infinity_X6882-user
+m bacon -j$(nproc --all)
