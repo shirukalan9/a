28e04e77a1d409b7d08ef4fd83cbd4995c834ea6
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-05-15 18:50:30 +0700

    h

---
+++ scripts/ax-x2.sh
@@ -0,0 +1,15 @@
+# Cleanup
+rm -rf device frameworks external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
+
+# ROM Init
+repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault --depth=1 --git-lfs --no-repo-verify
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+/opt/crave/resync.sh
+
+# Device 
+git clone -b stable-ax  https://github.com/testingprjct/8dua device/infinix/X6882
+
+# Build
+source build/envsetup.sh
+axion X6882 user pico
+ax -br 
